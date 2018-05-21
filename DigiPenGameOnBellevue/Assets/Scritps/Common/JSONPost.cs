9871d1e219b2cee1487f00f7fd182adad0810ca1
autisticvegan <vehman@gmail.com>
2018-05-20 23:05:04 -0700

    json post stub func

---
+++ DigiPenGameOnBellevue/Assets/Scritps/Common/JSONPost.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class JSONPost : MonoBehaviour {
+
+    private static readonly string POSTAddUserURL = "INSERTLATER";
+
+
+
+
+    public WWW POST()
+    {
+        WWW www;
+        Hashtable postHeader = new Hashtable();
+        postHeader.Add("Content-Type", "application/json");
+
+        string jsonStr = "\"\" "; //dont put newlines
+
+
+    // convert json string to byte
+    var formData = System.Text.Encoding.UTF8.GetBytes(jsonStr);
+
+        www = new WWW(POSTAddUserURL, formData, postHeader);
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+
+
+    IEnumerator WaitForRequest(WWW data)
+    {
+        yield return data; // Wait until the download is done
+        if (data.error != null)
+        {
+            Debug.Log("There was an error sending request: " + data.error);
+        }
+        else
+        {
+            Debug.Log("WWW Request: " + data.text);
+        }
+    }
+}
