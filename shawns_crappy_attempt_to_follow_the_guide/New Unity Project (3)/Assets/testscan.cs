049ba1dabb078d11f73989c2df9a5e20c192b5da
autisticvegan <vehman@gmail.com>
2018-05-29 23:13:16 -0700

    well most of this stuff is probably unnecessary but i dont know how to fix it so here it all is!

---
+++ shawns_crappy_attempt_to_follow_the_guide/New Unity Project (3)/Assets/testscan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BitToys))]
+public class testscan : MonoBehaviour {
+
+    public Text text;
+    
+    // Use this for initialization
+    void Start () {
+
+        BitToys.inst.Init("testprojectlol");
+        BitToys.inst.ble_QuickConnectReader();
+        BitToys.inst.onClaimToy_OK += OnClaimToy_OKEventHandler;
+        BitToys.inst.onClaimToy_Fail += OnClaimToy_FAIL;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void OnClaimToy_OKEventHandler(BitToys.Toy toy, bool TLL_val)
+    {
+
+    }
+
+    private void OnClaimToy_FAIL(BitToys.FailReason reason, string texto)
+    {
+        text.text = reason.ToString();
+        text.text += "\n";
+        text.text += texto;
+    }
+
+
+}
