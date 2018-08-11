a4e5343ebe73cb4058e880fb74310b89f409e99e
autisticvegan <vehman@gmail.com>
2018-08-10 18:43:36 -0700

    i put the fbx in but it doesnt animate because im bad lol

---
+++ GameOnRedmond566/Assets/spin2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin2 : MonoBehaviour
+{
+
+    int speed;
+    // Use this for initialization
+    void Start()
+    {
+        speed = Random.Range(0, 69);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
