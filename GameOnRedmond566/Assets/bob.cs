ce4c6706ba0f205933f05c9cb0273bbcb34c807a
autisticvegan <vehman@gmail.com>
2018-08-16 18:54:41 -0700

    bob script

---
+++ GameOnRedmond566/Assets/bob.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bob : MonoBehaviour {
+
+    // Use this for initialization
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
